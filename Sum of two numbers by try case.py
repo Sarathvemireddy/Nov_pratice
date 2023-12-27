062c7b788a45250b66593317207578f72cfd54eb
Sarathvemireddy <sarath.vemireddy98@gmail.com>
2023-12-27 10:23:25 +0530

    Python

---
+++ Sum of two numbers by try case.py
@@ -0,0 +1,27 @@
+#Apporach : 1
+try : 
+    a = int(input())
+    b = int(input())
+except:
+    print("You have to enter numbers")
+else:
+    print(f"The sum of {a} and {b} is : {a + b}")
+
+
+#Apporach : 2
+try : 
+    a = int(input())
+    b = int(input())
+except ValueError  as VL:
+    print(f"You have to enter numbers :  {VL}")
+else:
+    print(f"The sum of {a} and {b} is : {a + b}")
+
+#Apporach : 3
+try :
+    a = int(input())
+    b = int(input())
+except ValueError:
+    raise ValueError(f"You have to input Numbers")
+else:
+    print(f"The sum of {a} and {b} is : {a+b}")
